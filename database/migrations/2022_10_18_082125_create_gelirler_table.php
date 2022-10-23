9c746a98ebe7256a7d572c5f87ddc09a65b9aadc
asaita <saitayyildiz@gmail.com>
2022-10-23 11:27:56 +0300

    authentication ready

--- database/migrations/2022_10_18_082125_create_gelirler_table.php
+++ database/migrations/2022_10_18_082125_create_gelirler_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTodoTable extends Migration
+class CreateGelirlerTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,18 @@ class CreateTodoTable extends Migration
      */
     public function up()
     {
-        Schema::create('todo', function (Blueprint $table) {
-            $table->id();
-            $table->string('todo'); 
-            $table->smallInteger('isDone');  
+        Schema::create('gelirler', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('gelirAd');
+            $table->decimal('gelirFiyat',8,2);
+            $table->integer('miktar');
+            $table->decimal('toplamFiyat');
             $table->integer('user_id')->unsigned();
 
-            $table->foreign('user_id')
-            ->references('id')
-            ->on('users')
-            ->onDelete('cascade');
-
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+
+
 
 
         });
@@ -37,6 +37,6 @@ class CreateTodoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todo');
+        Schema::dropIfExists('gelirler');
     }
 }
